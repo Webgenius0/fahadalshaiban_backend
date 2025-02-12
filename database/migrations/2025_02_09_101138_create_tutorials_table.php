fb4b13cf88c774319f9ec488883b7e4d05d2d409
MuhammadSagorwdpf <sagorwdpf@gmail.com>
2025-02-12 19:03:53 +0600

    Add comment for tutorials table creation in migration

--- database/migrations/2025_02_09_101138_create_tutorials_table.php
+++ database/migrations/2025_02_09_101138_create_tutorials_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+
+        // create tutorials table
         Schema::create('tutorials', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
